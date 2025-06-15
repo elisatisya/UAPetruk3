a815ab0c63e7539f5c694f4a4fe00a591b5e1ffd
elisatisya <elisatisya1@gmail.com>
2025-06-15 21:02:14 +0700

    Membuat library

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
+#include <stack>
+#include <queue>
+#include <map>
+#include <fstream>
+#include <windows.h>
+#include <cmath>
+
+
+using namespace std;
+
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
