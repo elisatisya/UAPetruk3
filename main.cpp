cee0802bbd0a89fcf1bce08b5672213492da093b
viandraagsatri <vagsatri@gmail.com>
2025-06-15 21:15:08 +0700

    Membuat function banding tanggal

--- main.cpp
+++ main.cpp
@@ -28,3 +28,19 @@ struct Pengeluaran {
     string kategori;
     double jumlah;
 };
+
+bool bandingTanggal(const string& t1, const string& t2) {
+    int d1 = stoi(t1.substr(0, 2));
+    int m1 = stoi(t1.substr(3, 2));
+    int y1 = stoi(t1.substr(6, 4));
+
+
+    int d2 = stoi(t2.substr(0, 2));
+    int m2 = stoi(t2.substr(3, 2));
+    int y2 = stoi(t2.substr(6, 4));
+
+
+    if (y1 != y2) return y1 < y2;
+    if (m1 != m2) return m1 < m2;
+    return d1 < d2;
+}
