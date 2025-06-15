ec6463eefb5a0daead5f32eebf0d038f1753c039
Alyssa <lisxph07@gmail.com>
2025-06-15 21:17:39 +0700

    Menambah fungsi untuk mencari tanggal

--- main.cpp
+++ main.cpp
@@ -11,9 +11,7 @@
 #include <windows.h>
 #include <cmath>
 
-
 using namespace std;
-
 
 void gotoxy(int x, int y) {
     COORD coord;
@@ -34,13 +32,28 @@ bool bandingTanggal(const string& t1, const string& t2) {
     int m1 = stoi(t1.substr(3, 2));
     int y1 = stoi(t1.substr(6, 4));
 
-
     int d2 = stoi(t2.substr(0, 2));
     int m2 = stoi(t2.substr(3, 2));
     int y2 = stoi(t2.substr(6, 4));
 
-
     if (y1 != y2) return y1 < y2;
     if (m1 != m2) return m1 < m2;
     return d1 < d2;
+}
+
+int cariTanggal(const vector<Pengeluaran>& data, const string& target, bool cariAwal) {
+    int kiri = 0, kanan = data.size() - 1, hasil = -1;
+    while (kiri <= kanan) {
+        int tengah = (kiri + kanan) / 2;
+        if ((cariAwal && !bandingTanggal(data[tengah].tanggal, target)) ||
+            (!cariAwal && !bandingTanggal(target, data[tengah].tanggal))) {
+            hasil = tengah;
+            if (cariAwal) kanan = tengah - 1;
+            else kiri = tengah + 1;
+        } else {
+            if (cariAwal) kiri = tengah + 1;
+            else kanan = tengah - 1;
+        }
+    }
+    return hasil;
 }
